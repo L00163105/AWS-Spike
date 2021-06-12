6778ab4ca4f3f99d6422651e3cbcd2a985a795e9
L00163105 <ronan.clancy@gmail.com>
2021-06-12 14:26:57 +0100

    intial workflow

---
+++ src/main/java/com/ronan/githubactionsspike/GithubactionsspikeApplication.java
@@ -0,0 +1,13 @@
+package com.ronan.githubactionsspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionsspikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionsspikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronan/githubactionsspike/GithubactionsspikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronan.githubactionsspike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionsspikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
